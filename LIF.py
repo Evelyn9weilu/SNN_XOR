0f1f1e18052aaf2c86027eabf94876a4b7744377
jakezhou <jakezhou>
2020-04-17 20:35:09 -0400

    Set up the LIF neuron fully

--- LIF.py
+++ LIF.py
@@ -1,26 +1,45 @@
 
 class LIF_Neuron():
 
-    currCharge = 0
+    # constant LIF properties
+    # baseline charge when neuron is fully neutral
+    baseCharge = 1
+    # membrane capacitance
+    C = 1
+    # time interval for our neuron, based on neuron firing 200 times per sec
+    dt = 1
+    # neuron resistance
+    resistance = 12
+    # spiking threshold
+    threshold = 10
+    # additional charge that results from passing threshold
+    spike = 5
 
-    def __init__(self):
-        self.currCharge = 1
+    # dynamic LIF variables
+    inputCharge = 0
+    currentCharge = baseCharge
 
-    def runNeuron(self, inputCharge):
+    # Constructor takes in initial input charge
+    def __init__(self, current):
+        self.inputCharge = current
 
-        resistance = 100
-        threshold = 10
-        spike = 5
-        C = 1
+    # Every call using .start() or next() will update the neuron charge
+    # Neuron will check if it should spike
+    # If it has spiked, we yield a 1 and the current charge plus spike
+    # otherwise we yield a 0 and the baseline charge
+    def start(self):
 
-        self.currCharge += (inputCharge - (self.currCharge / resistance)) / C
-        if self.currCharge  < 0:
-            self.currCharge = 0
-        if (self.currCharge >= threshold):
-            # currCharge + spike
-            # currCharge = float(0)
-            self.currCharge = C
-            
-            return True
+        # add dV to current charge, dv based on input charge and leak
+        self.currentCharge += self.dt * (self.inputCharge - (self.currentCharge / self.resistance)) / self.C
 
-        return False
+        # Check if we have reached the threshold
+        if self.currentCharge >= self.threshold:
+            # resetting charge and spiking
+            self.currentCharge = self.baseCharge
+            yield (1, self.currentCharge + self.spike)
+        else:
+
+
+            # IF THIS DOESN'T WORK, TRY OUTPUTTING 90% OF THE PREVIOUS OUTPUT
+
+            yield (0, self.baseCharge)
--- XORNet.py
+++ XORNet.py
@@ -2,17 +2,17 @@ from LIF import LIF_Neuron
 import numpy as np
 import math
 
+
+# Neuron 0 and 1 are input neurons. Neurons 2 and 3 are hidden layer neurons. Neuron 4 is the output neuron
+
 # 2 input neurons, 1 for first num and another for second num
-inputNeurons = [LIF_Neuron()] * 2
+inputNeurons = [LIF_Neuron(0)] * 2
 # 2 hidden neurons, one representing or and one representing nand
-middleNeurons = [LIF_Neuron()] * 2
+middleNeurons = [LIF_Neuron(0)] * 2
 # 1 output neuron, fires high if XOR, fires low if !XOR
-outputNeuron = LIF_Neuron()
+outputNeuron = LIF_Neuron(0)
 
 # Creating random initial weights
-weights = np.random.random(6)
-print("Initial random weights: " + weights)
-# Neuron 0 and 1 are input neurons. Neurons 2 and 3 are hidden layer neurons. Neuron 4 is the output neuron
 # Weights represent certain edges in this network:
 # 0 (0,2) input0 to OR
 # 1 (0,3) input0 to NAND
@@ -20,128 +20,59 @@ print("Initial random weights: " + weights)
 # 3 (1,3) input1 to NAND
 # 4 (2,4) OR to output
 # 5 (3,4) NAND to output
+weights = np.random.random(6)
+print("Initial random weights: " + weights)
 
-# High input = 1 ???
-# Low input <= .5 ???
-lowInput = 2
-highInput = 7
-inputs = [.35] *2
+# Creating constants for our high and low inputs to represent 0 or 1
+LOW_INPUT = 2
+HIGH_INPUT = 11
+
+# Creating tuples for our inputs
+inputs = [(0, 0), (0, 1), (1, 0), (1, 1)]
+
+# Constants for our weight change
+ALPHA = 1
 
 
 def train():
-
-    for k in range(4):
-        if (k == 0):
-            inputs[0] = lowInput
-            inputs[1] = highInput
-            trainingInput= 1000
-        if (k == 1):
-            inputs[0] = highInput
-            inputs[1] = lowInput
-            trainingInput= 1000
-        if (k == 2):
-            inputs[0] = lowInput
-            inputs[1] = lowInput
-            trainingInput = -50
-        if (k == 3):
-            inputs[0] = highInput
-            inputs[1] = highInput
-            trainingInput = -50
-
-        for i in range(3000):
-            
-            # outputNeuron.runNeuron(trainingInput)
-            for j in range (len(inputNeuron)):
-                
-                if ((inputNeuron[j].runNeuron(inputs[j]))):
-                    
-                    nextInput = 1
-                else:
-                    nextInput = 0
-                for i in range (len(middleNeuron)):
-                    if (middleNeuron[i].runNeuron(nextInput * weights[j*4 + i])):
-                        
-                        #strengthen weights here
-                        weights[j*4 + i] += (alpha * weights[j*4 + i]) - decay 
-                        toOutput = 1
-                    else:
-                        weights[j*4 + i] -= decay 
-                        toOutput = 0
-                    if (outputNeuron.runNeuron(trainingInput * weights[i+8] + (toOutput * weights[i + 8]))):
-                        
-                        weights[i+8] += (alpha * weights[i+8]) - decay 
-                    else:
-                        weights[i+8] -= decay 
+    # Train network 5,000 times
+    for j in range(5000):
+        # Train network for each input
+        for i in inputs:
+            if i == (0, 0):
+                inputNeurons[0] = LIF_Neuron(LOW_INPUT)
+                inputNeurons[1] = LIF_Neuron(LOW_INPUT)
+            elif i == (0, 1):
+                inputNeurons[0] = LIF_Neuron(LOW_INPUT)
+                inputNeurons[1] = LIF_Neuron(HIGH_INPUT)
+            elif i == (1, 0):
+                inputNeurons[0] = LIF_Neuron(HIGH_INPUT)
+                inputNeurons[1] = LIF_Neuron(LOW_INPUT)
+            elif i == (1, 1):
+                inputNeurons[0] = LIF_Neuron(HIGH_INPUT)
+                inputNeurons[1] = LIF_Neuron(HIGH_INPUT)
+    return
 
 
 def test(x, y):
-    neuron1Fires = 0
-    neuron2Fires = 0
-    totalFires = 0
-    if x == 0:
-        input0 = lowInput
-    else:
-        input0 = highInput
-    if y == 0:
-        input1 = lowInput
-    else:
-        input1 = highInput
-    for i in range(500):
-        
-        if (inputNeuron[0].runNeuron(input0)):
-            nextInput = 1
-        else:
-            nextInput = 0
-        for i in range (len(middleNeuron)):
-            if (middleNeuron[i].runNeuron(nextInput * weights[i])):
-                toOutput = 1
-            else:
-                toOutput = 0
-            if (outputNeuron.runNeuron(toOutput * weights[i + 8])):
-                totalFires+=1
-        if (inputNeuron[1].runNeuron(input0)):
-            nextInput = 1
-        else:
-            nextInput = 0
-        for i in range (len(middleNeuron)):
-            if (middleNeuron[i].runNeuron(nextInput * weights[i+4])):
-                toOutput = 1
-            else:
-                toOutput = 0
-            if (outputNeuron.runNeuron(toOutput * weights[i + 8])):
-                totalFires+=1
-    
-    return totalFires
+    if x == 0 and y == 0:
+        inputNeurons[0] = LIF_Neuron(LOW_INPUT)
+        inputNeurons[1] = LIF_Neuron(LOW_INPUT)
+    elif x == 0 and y == 1:
+        inputNeurons[0] = LIF_Neuron(LOW_INPUT)
+        inputNeurons[1] = LIF_Neuron(HIGH_INPUT)
+    elif x == 1 and y == 0:
+        inputNeurons[0] = LIF_Neuron(HIGH_INPUT)
+        inputNeurons[1] = LIF_Neuron(LOW_INPUT)
+    elif x == 1 and y == 1:
+        inputNeurons[0] = LIF_Neuron(HIGH_INPUT)
+        inputNeurons[1] = LIF_Neuron(HIGH_INPUT)
+    return
 
 
 train()
 print(weights)
-print(test(0,0))
-print(test(0,1))
-print(test(1,0))
-print(test(1,1))
-
-
-# def train(input):
-#     for i in range(1000):
-#         if (input == (0,0)):
-#             inputNeuron[0].runNeuron(lowInput)
-#             inputNeuron[1].runNeuron(lowInput)
-#         if (input == (0,1)):
-#             inputNeuron[0].runNeuron(lowInput)
-#             inputNeuron[1].runNeuron(highInput)
-#         if (input == (1,0)):
-#             inputNeuron[0].runNeuron(highInput)
-#             inputNeuron[1].runNeuron(lowInput)
-#         if (input == (1,1)):
-#             inputNeuron[0].runNeuron(highInput)
-#             inputNeuron[1].runNeuron(highInput) 
-            
-# # Things to figure out
-# # How to tell if two neurons are firing at the same time
-
-# # Brain Damage's result for (0,0), (0,1), (1,0), (1,1)
-# (0, 296, 184)
-# (28, 294, 190)
-# (27, 294, 190)
-# (66, 265, 185)
+print(test(0, 0))
+print(test(0, 1))
+print(test(1, 0))
+print(test(1, 1))
